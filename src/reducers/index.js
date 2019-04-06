66da635752d6f57e652399e5f9e6567e24744c5c
SolderingKnowledge <aikoltashbolotuulu@Aikols-MacBook-Pro.local>
2019-04-05 22:43:50 -0700

    second

---
+++ src/reducers/index.js
@@ -0,0 +1,287 @@
+const initialState = {
+    events: {
+        native: {
+            january: {
+                1: [
+                    {
+                        type: "native",
+                        name: "New Year’s Day"
+                    }
+                ],
+                20: [
+                    {
+                        type: "native",
+                        name: "Martin Luther King, Jr. Day"
+                    }
+                ]
+            },
+            february: {
+                2: [
+                    {
+                        type: "native",
+                        name: "Groundhog Day"
+                    }
+                ],
+                5: [
+                    {
+                        type: "native",
+                        name: "Lunar New Year"
+                    }
+                ],
+                14: [
+                    {
+                        type: "native",
+                        name: "Valentine’s Day"
+                    }
+                ],
+                18: [
+                    {
+                        type: "native",
+                        name: "President's Day"
+                    }
+                ]
+            },
+            march: {
+                10: [
+                    {
+                        type: "native",
+                        name: "Daylight Saving Time"
+                    }
+                ],
+                17: [
+                    {
+                        type: "native",
+                        name: "St. Patrick’s Day"
+                    }
+                ],
+                21: [
+                    {
+                        type: "native",
+                        name: "Holi"
+                    }
+                ]
+            },
+            april: {
+                1: [
+                    {
+                        type: "native",
+                        name: "April Fools' Day"
+                    }
+                ],
+                14: [
+                    {
+                        type: "native",
+                        name: "Palm Sunday"
+                    }
+                ],
+                15: [
+                    {
+                        type: "native",
+                        name: "Tax Day"
+                    }
+                ],
+                19: [
+                    {
+                        type: "native",
+                        name: "Good Friday"
+                    }
+                ],
+                20: [
+                    {
+                        type: "native",
+                        name: "Passover"
+                    }
+                ],
+                21: [
+                    {
+                        type: "native",
+                        name: "Easter"
+                    }
+                ],
+                22: [
+                    {
+                        type: "native",
+                        name: "Earth Day"
+                    }
+                ],
+                28: [
+                    {
+                        type: "native",
+                        name: "Orthodox Easter"
+                    }
+                ]
+            },
+            may: {
+                5: [
+                    {
+                        type: "native",
+                        name: "Cinco de Mayo"
+                    },
+                    {
+                        type: "native",
+                        name: "First Day of Ramadan"
+                    }
+                ],
+                12: [
+                    {
+                        type: "native",
+                        name: "Mother’s Day"
+                    }
+                ],
+                27: [
+                    {
+                        type: "native",
+                        name: "Memorial Day"
+                    }
+                ]
+            },
+            june: {
+                3: [
+                    {
+                        type: "native",
+                        name: "Eid al-Fitr"
+                    }
+                ],
+                14: [
+                    {
+                        type: "native",
+                        name: "Flag Day"
+                    }
+                ],
+                16: [
+                    {
+                        type: "native",
+                        name: "Father’s Day"
+                    }
+                ],
+                19: [
+                    {
+                        type: "native",
+                        name: "Juneteenth"
+                    }
+                ]
+            },
+            july: {
+                4: [
+                    {
+                        type: "native",
+                        name: "Independence Day"
+                    }
+                ]
+            },
+            august: {
+                10: [
+                    {
+                        type: "native",
+                        name: "Eid al-Adha"
+                    }
+                ]
+            },
+            september: {
+                2: [
+                    {
+                        type: "native",
+                        name: "Labor Day"
+                    }
+                ],
+                9: [
+                    {
+                        type: "native",
+                        name: "Ashura"
+                    }
+                ],
+                30: [
+                    {
+                        type: "native",
+                        name: "Rosh Hashanah"
+                    }
+                ]
+            },
+            october: {
+                9: [
+                    {
+                        type: "native",
+                        name: "Yom Kippur"
+                    }
+                ],
+                14: [
+                    {
+                        type: "native",
+                        name: "Columbus Day"
+                    },
+                    {
+                        type: "native",
+                        name: "Indigenous Peoples' Day"
+                    }
+                ],
+                27: [
+                    {
+                        type: "native",
+                        name: "Diwali"
+                    }
+                ],
+                31: [
+                    {
+                        type: "native",
+                        name: "Halloween"
+                    }
+                ]
+            }
+			november: {
+                3: [
+                    {
+                        type: "native",
+                        name: "Daylight Saving Time End"
+                    }
+                ],
+                11: [
+                    {
+                        type: "native",
+                        name: "Veterans Day"
+                    }
+                ],
+                28: [
+                    {
+                        type: "native",
+                        name: "Thanksgiving"
+                    }
+                ]
+            },
+            december: {
+                23: [
+                    {
+                        type: "native",
+                        name: "Hanukkah"
+                    }
+                ],
+                24: [
+                    {
+                        type: "native",
+                        name: "Christmas Eve"
+                    }
+                ],
+                25: [
+                    {
+                        type: "native",
+                        name: "Christmas Day"
+                    }
+                ],
+                26: [
+                    {
+                        type: "native",
+                        name: "Kwanzaa"
+                    }
+                ],
+                31: [
+                    {
+                        type: "native",
+                        name: "New Year’s Eve"
+                    }
+                ]
+            }
+        },
+        custom: {
+
+        }
+    }
+}
